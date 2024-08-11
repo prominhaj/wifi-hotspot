4cd3ab7897894ec53e32d2ee881302bd63cfbb80
prominhaj <parsonal322532@gmail.com>
2024-08-11 20:32:09 +0600

    Fix force-dynamic problem

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import CreateUser from '@/components/globals/CreateUser/CreateUser';
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
     return (
         <main className='flex items-center justify-center h-screen'>
