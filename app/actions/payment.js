f899c400c9e81f14dfbdc1838c2f8f45629f6e2b
pro-minhaj <parsonal322532@gmail.com>
2024-08-15 00:42:05 +0600

    Change packagePayment base url

--- app/actions/payment.js
+++ app/actions/payment.js
@@ -2,16 +2,13 @@
 
 export const packagePayment = async (amount, userId) => {
     try {
-        const response = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_URL}/api/bkash/payment/create`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ amount, userId })
-            }
-        );
+        const response = await fetch(`${process.env.BASE_URL}/api/bkash/payment/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount, userId })
+        });
         const { bkashURL } = await response.json();
         return bkashURL;
     } catch (error) {
