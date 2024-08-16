2589e76235ff57afbc2b88e95d5d1c59db4970d0
pro-minhaj <parsonal322532@gmail.com>
2024-08-16 19:35:21 +0600

    Implement PWA in Destop Device And Mobile Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
+import withPWA from '@ducanh2912/next-pwa';
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+    dest: 'public',
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: false,
+    workboxOptions: {
+        disableDevLogs: true
+    }
+})(nextConfig);
