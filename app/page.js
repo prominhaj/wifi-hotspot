e737046f32d20de873ecf7110336acea0c11297b
pro-minhaj <parsonal322532@gmail.com>
2024-08-19 04:56:45 +0600

    update home page redirect dashboard page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,7 @@
+import { redirect } from 'next/navigation';
+
 const HomePage = async () => {
-    return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolorum veniam eum
-            delectus maxime veritatis tempora fuga dicta, autem ea architecto expedita doloremque
-            et, nam qui voluptatem pariatur sequi reiciendis quis dolore? Tempora nobis, vel nemo
-            sapiente nostrum aperiam, vitae voluptatem et officiis numquam tempore est? Error
-            voluptas veniam id!
-        </div>
-    );
+    redirect('/dashboard');
 };
 
 export default HomePage;
