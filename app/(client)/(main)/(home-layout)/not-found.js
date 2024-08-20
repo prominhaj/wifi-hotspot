68a45dccdc9cdf9986dac8d7a07fd72c7ef22ddd
pro-minhaj <parsonal322532@gmail.com>
2024-08-21 01:16:34 +0600

    Delete Extra Error And Not Found Page

--- app/(client)/(main)/(home-layout)/not-found.js
+++
@@ -1,9 +0,0 @@
-import NotFound from '@/components/globals/NotFound/NotFound';
-
-export default function ClientLayoutNotFoundPage() {
-    return (
-        <div className='flex min-h-[80vh] flex-col items-center justify-center bg-background'>
-            <NotFound />
-        </div>
-    );
-}
