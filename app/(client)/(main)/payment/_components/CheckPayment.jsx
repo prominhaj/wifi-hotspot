0a90e514a04852a9f37d777dbcdf1580cdb83f24
pro-minhaj <parsonal322532@gmail.com>
2024-08-26 10:42:11 +0600

    Fix env file problem

--- app/(client)/(main)/payment/_components/CheckPayment.jsx
+++ app/(client)/(main)/payment/_components/CheckPayment.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const CheckPayment = ({ user }) => {
     return (
         <div className='flex flex-col items-center justify-center flex-1 w-full h-full my-16'>
-            <div className='flex flex-col items-center max-w-full gap-6 text-center'>
+            <div className='flex flex-col items-center max-w-full gap-6 text-center bello'>
                 <>
                     <CircleCheck className='w-32 h-32 p-0 text-white bg-green-500 rounded-full' />
                     <h1 className='text-xl md:text-2xl lg:text-3xl'>
