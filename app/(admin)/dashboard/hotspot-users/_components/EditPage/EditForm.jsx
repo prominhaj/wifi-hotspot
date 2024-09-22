b5869f3f441cb8e38b1704d2e762e97827001843
Md Rakibul Islam <bdsky25@gmail.com>
2024-09-23 00:36:31 +0600

    Add Expiry date update on edit user

--- app/(admin)/dashboard/hotspot-users/_components/EditPage/EditForm.jsx
+++ app/(admin)/dashboard/hotspot-users/_components/EditPage/EditForm.jsx
@@ -1,47 +1,67 @@
-"use client";
-import SubmitButton from "@/components/globals/SubmitButton/SubmitButton";
-import FormControl from "./FormControl";
-import { useCallback } from "react";
-import { toast } from "sonner";
-import { updateHotspotUserById } from "@/app/actions/hotspotUser";
-import { useRouter } from "next/navigation";
+'use client';
+import SubmitButton from '@/components/globals/SubmitButton/SubmitButton';
+import FormControl from './FormControl';
+import { useCallback } from 'react';
+import { toast } from 'sonner';
+import { updateHotspotUserById } from '@/app/actions/hotspotUser';
+import { useRouter } from 'next/navigation';
 
 const EditForm = ({ editHotspotUser }) => {
     const { back } = useRouter();
 
-    const handleUpdateHotspotUser = useCallback(async (formData) => {
-        const username = formData.get('username');
-        const password = formData.get('password');
+    const handleUpdateHotspotUser = useCallback(
+        async (formData) => {
+            const username = formData.get('username');
+            const password = formData.get('password');
+            const expiredAt = formData.get('expiry-date');
 
-        try {
-            const updatedInfo = {
-                username,
-                password,
+            try {
+                const updatedInfo = {
+                    username,
+                    password,
+                    expiredAt
+                };
+                const result = await updateHotspotUserById(
+                    editHotspotUser?.id,
+                    editHotspotUser?.hotspotUserId,
+                    updatedInfo
+                );
+                if (result.success) {
+                    toast.success('Hotspot user updated successfully');
+                    back();
+                } else {
+                    toast.error('Failed to update hotspot user');
+                }
+            } catch (error) {
+                toast.error(error.message);
             }
-            const result = await updateHotspotUserById(editHotspotUser?.id, editHotspotUser?.hotspotUserId, updatedInfo);
-            if (result.success) {
-                toast.success("Hotspot user updated successfully");
-                back();
-            } else {
-                toast.error("Failed to update hotspot user");
-            }
-        } catch (error) {
-            toast.error(error.message)
-        }
-    }, [editHotspotUser?.hotspotUserId, editHotspotUser?.id, back])
+        },
+        [editHotspotUser?.hotspotUserId, editHotspotUser?.id, back]
+    );
 
     return (
-        <form action={handleUpdateHotspotUser} className="grid grid-cols-1 gap-3">
-            <FormControl defaultValue={editHotspotUser?.username} name="username">
+        <form action={handleUpdateHotspotUser} className='grid grid-cols-1 gap-3'>
+            <FormControl defaultValue={editHotspotUser?.username} name='username'>
                 Username
             </FormControl>
-            <FormControl defaultValue={editHotspotUser?.password} name="password">
+            <FormControl defaultValue={editHotspotUser?.password} name='password'>
                 Password
             </FormControl>
-            <FormControl disabled={true} defaultValue={editHotspotUser?.macAddress} name="mac-address">
+            <FormControl
+                disabled={true}
+                defaultValue={editHotspotUser?.macAddress}
+                name='mac-address'
+            >
                 Mac Address
             </FormControl>
-            <SubmitButton className="mt-1.5" variant="primary" size="sm">
+            <FormControl
+                disabled={false}
+                defaultValue={editHotspotUser?.expiredAt}
+                name='expiry-date'
+            >
+                Expiry Date
+            </FormControl>
+            <SubmitButton className='mt-1.5' variant='primary' size='sm'>
                 Update Details
             </SubmitButton>
         </form>
