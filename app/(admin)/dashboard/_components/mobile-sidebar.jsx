556a9413c41824535f36bbc289f506f1ae48c878
Md Minhaj <139013851+prominhaj@users.noreply.github.com>
2024-09-27 00:30:31 +0600

    Update mobile-sidebar.jsx

--- app/(admin)/dashboard/_components/mobile-sidebar.jsx
+++ app/(admin)/dashboard/_components/mobile-sidebar.jsx
@@ -9,7 +9,7 @@ export const MobileSidebar = () => {
 
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
-      <SheetTrigger className="pr-4 transition lg:hidden hover:opacity-75">
+      <SheetTrigger className="pr-4 transition lg:hidden hover:opacity-75 bellotest">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-background/50">
