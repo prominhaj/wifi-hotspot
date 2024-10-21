817e2aca64a516ad519c5404a57179690e04b1e2
pro-minhaj <parsonal322532@gmail.com>
2024-10-21 23:14:57 +0600

    Fix Update Mac Address In Admin Panel

--- app/actions/admin.js
+++ app/actions/admin.js
@@ -1,25 +1,58 @@
 'use server';
 
 import { deleteFile } from '@/lib/fileUploader';
+import HotspotUser from '@/modals/hotspot-user-modal';
+import MacAddress from '@/modals/mac-address-modal';
 import User from '@/modals/user-modal';
+import { getHotspotUserByUsername } from '@/queries/mikrotik';
 import { revalidatePath } from 'next/cache';
 
 export const updateHotspotUsersMacAddress = async () => {
     try {
-        const response = await fetch(`${process.env.BASE_URL}/api/update-mac-address`);
-        const result = await response.json();
-        if (result?.success) {
-            // revalidatePath
-            revalidatePath('/');
+        // Fetch all no macAddress hotspot users
+        const getDataBaseHotspotUsers = await HotspotUser.find({
+            $or: [
+                { macAddress: { $exists: false } },
+                {
+                    macAddress: ''
+                }
+            ]
+        }).lean();
 
-            return {
-                success: true,
-                message: result.message
-            };
+        for (const databaseHotspotUser of getDataBaseHotspotUsers) {
+            const hotspotUser = await getHotspotUserByUsername(databaseHotspotUser?.username, true);
+            const macAddress = hotspotUser?.['mac-address'];
+
+            // Update the Hotspot user set macAddress in the database
+            if (macAddress) {
+                await HotspotUser.updateOne(
+                    {
+                        _id: databaseHotspotUser._id,
+                        status: 'active'
+                    },
+                    {
+                        macAddress
+                    }
+                );
+
+                // update macAddress in this connection
+                const existingMacAddress = await MacAddress.exists({ macAddress });
+                if (!existingMacAddress) {
+                    await MacAddress.create({
+                        hotspotUserId: databaseHotspotUser._id,
+                        userId: databaseHotspotUser?.userId,
+                        macAddress
+                    });
+                }
+            }
         }
+
+        // revalidatePath
+        revalidatePath('/');
+
         return {
-            success: false,
-            message: result.message
+            success: true,
+            message: 'Update MacAddress Successfully'
         };
     } catch (error) {
         throw new Error(error);
